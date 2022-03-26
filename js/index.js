ee149e804626ad159168d9b9d0b79e31ff0a1c2c
TyomaKirakos <98010508+TyomaKirakos@users.noreply.github.com>
2022-03-27 02:37:13 +0300

    made some html and css

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// const loginBtn = document.querySelector('.head-btns__login-btn');
+// console.log('abopgfoih');
+
+// loginBtn.addEventListener('click', () => {
+//     window.location.href = '../login.html';
+// });
